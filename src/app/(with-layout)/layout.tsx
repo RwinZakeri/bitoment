43d2cbc6368fb4abb2a2676045f60b58c192dba8
RwiinZakeri <rwiiinzakeri@gmail.com>
2025-10-21 19:28:56 +0330

    update

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -12,12 +12,11 @@ import { v4 as uuid } from "uuid";
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-
   const navItems = [
     { href: "/dashboard", icon: <GridIcon /> },
     { href: "/wallet", icon: <WalletIcon /> },
     { href: "/", icon: <BarChartSquareIcon /> },
-    { href: "/prswapofile", icon: <SwapIcon /> },
+    { href: "/swap", icon: <SwapIcon /> },
     { href: "/profile", icon: <ProfileIcon /> },
   ];
 
@@ -28,13 +27,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <nav className="fixed left-1/2 -translate-x-1/2 bottom-0 max-w-[520px] w-full h-[70px] bg-white shadow-xl flex justify-around items-center">
         {navItems.map((item) => {
           const isActive = `/${pathname.split("/")[1]}` == item.href;
-          console.log(pathname.split("/")[1] , item.href)
+          console.log(pathname.split("/")[1], item.href);
           return (
             <Link
               key={uuid()}
               href={item.href}
               className={`flex flex-col items-center justify-center gap-1 w-full h-full transition-all ${
-                isActive ? "text-blue-500" : "text-gray-500"
+                isActive ? "text-black" : "text-gray-500"
               }`}
             >
               {item.icon}
